6cdb973cd14f00d108262a8bee5880c1cbb0204b
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-11 16:07:28 +0000

    HACKING-MD

---
+++ commandes/dalle.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const { zokou } = require("../framework/zokou");
+
+zokou({
+  nomCom: "dalle2",
+  description: "Génère une image en utilisant l'API Photoleap",
+  usage: "generate-image <nom de l'image>",
+  categorie: "IA",
+  reaction: "✨"
+}, async (origineMessage, zk, commandeOptions) => {
+  const { repondre, arguments } = commandeOptions;
+
+  if (arguments.length === 0) {
+    repondre("Veuillez spécifier un nom pour l'image à générer.");
+    return;
+  }
+
+  const imageName = arguments.join(" ");
+
+  try {
+    // Appel à l'API Photoleap pour générer l'image
+    const response = await axios.get(`https://api.maher-zubair.tech/ai/photoleap?q=${encodeURIComponent(imageName)}`);
+
+    // Vérifier si la réponse contient une URL d'image
+    if (response.data && response.data.url) {
+      const imageUrl = response.data.url;
+
+      // Envoyer l'image générée dans le chat
+      await zk.envoyerMessage(origineMessage, { image: imageUrl });
+    } else {
+      repondre("Une erreur s'est produite lors de la génération de l'image.");
+    }
+  } catch (error) {
+    console.error(error);
+    repondre("Une erreur s'est produite lors de la génération de l'image.");
+  }
+});
