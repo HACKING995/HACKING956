29fba4971bc4af8548c7c28d57a088abdd14f69b
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-12 11:18:47 +0000

    HACKING-MD

--- commandes/igdl-fb-tk.js
+++ commandes/igdl-fb-tk.js
@@ -126,3 +126,59 @@ async (dest, zk, commandeOptions) => {
     repondre('Erreur lors du téléchargement de la vidéo.' , error);
   }
 });
+
+zokou({ nomCom: "tiktok2", categorie: "Download", reaction: "🎵" }, async (dest, zk, commandeOptions) => {
+  const { arg, ms, prefixe, repondre } = commandeOptions;
+  if (!arg[0]) {
+    repondre(`how to use this command:\n ${prefixe}tiktok tiktok_video_link`);
+    return;
+  }
+
+  const videoUrl = arg.join(" ");
+
+  const axios = require('axios');
+  const cheerio = require('cheerio');
+
+  async function tiktokdlF(url) {
+    if (!/tiktok/.test(url)) {
+      return `_*Thomas TIKTOK DL*_\n\n*_Past a tiktok link._*\n\n*_Example:_* _${prefixe}tiktok Url here_`;
+    }
+
+    const gettoken = await axios.get('https://tikdown.org/id');
+    const $ = cheerio.load(gettoken.data);
+    const token = $('#download-form > input[type=hidden]:nth-child(2)').attr('value');
+    const param = { url: url, _token: token };
+
+    const { data } = await axios.post('https://tikdown.org/getAjax?', new URLSearchParams(param), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
+      }
+    });
+
+    const getdata = cheerio.load(data.html);
+    if (data.status) {
+      return {
+        status: true,
+        thumbnail: getdata('img').attr('src'),
+        video: getdata('div.download-links > div:nth-child(1) > a').attr('href'),
+        audio: getdata('div.download-links > div:nth-child(2) > a').attr('href')
+      };
+    } else {
+      return { status: false };
+    }
+  }
+
+  const result = await tiktokdlF(videoUrl);
+
+  if (result.status) {
+    const caption = `
+Author: ${result.author}
+Description: ${result.desc}
+      `;
+
+    zk.sendMessage(dest, { video: { url: result.video }, caption: caption }, { quoted: ms });
+  } else {
+    zk.sendMessage(dest, `Failed to download TikTok video. Please make sure the provided URL is valid.`, { quoted: ms });
+  }
+});
