254f2b553e2ab0b4a921af6c179017ea63bbb367
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-21 20:40:54 +0000

    HACKING-MD

--- commandes/dalle2.js
+++ commandes/dalle2.js
@@ -1,5 +1,5 @@
 const { zokou } = require('../framework/zokou');
-const  axios  = require('axios');
+const axios = require('axios');
 
 zokou({ nomCom: "dalle2", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
   const { repondre, arg, ms } = commandeOptions;
@@ -9,36 +9,36 @@ zokou({ nomCom: "dalle2", reaction: "📡", categorie: "IA" }, async (dest, zk, 
       return repondre(`Please enter the necessary information to generate the image.`);
     }
 
-    // Regrouper les arguments en une seule chaîne séparée par "-"
-    
-   let text2i = await genimage(arg)
-    
+    const text2i = await genimage(arg);
+
     let caption = '*powered by Hacking-MD*';
 
-    // Utiliser la méthode sendMessageWithMedia pour envoyer l'image en tant que buffer
     zk.sendMessageWithMedia(dest, { buffer: text2i }, { caption: caption, quoted: ms });
   } catch (error) {
-    console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+    console.error('Error:', error.message || 'An error occurred');
+    if (error.response) {
+      console.error('Status code:', error.response.status);
+      console.error('Response data:', error.response.data);
+    }
     repondre("Oops, an error occurred while processing your request");
   }
 });
 
-
 async function genimage(args) {
-const data = {
-  model: "majicmixRealistic_v4",
-  prompt: args,
-  negative_prompt: "Do not put text or writing on the picture",
-  style: "anime",
-  sampler: "DPM++ 2M Karras"
-};
+  const data = {
+    model: "majicmixRealistic_v4",
+    prompt: args,
+    negative_prompt: "Do not put text or writing on the picture",
+    style: "anime",
+    sampler: "DPM++ 2M Karras"
+  };
 
-const response = await axios.post('https://api.onesytex.my.id/api/text2image/generate-image', data, {
-  headers: {
-    'accept': '*/*',
-    'Content-Type': 'application/json'
-  }
-})
+  const response = await axios.post('https://api.onesytex.my.id/api/text2image/generate-image', data, {
+    headers: {
+      'accept': '*/*',
+      'Content-Type': 'application/json'
+    }
+  });
 
-return response.data
+  return response.data;
 }
