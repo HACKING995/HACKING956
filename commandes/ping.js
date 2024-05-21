9037cb1d87a98eab7afdd7d99ee1d3e7052fdc93
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-05-21 20:42:32 +0000

    HACKING-MD

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Abrotech!!!*\n ```' + 7000 + '``` *𝐌𝐬*') 
+    return repondre('*THOMAS TECH!!!*\n ```' + 9000 + '``` *𝐌𝐬*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
