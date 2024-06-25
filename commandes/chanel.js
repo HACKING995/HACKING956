0a9c7138e3409568a1e38a673487f0f67986a03f
Bot WhatsApp HACKING <154031706+HACKING995@users.noreply.github.com>
2024-06-25 19:57:37 +0000

    𝐇𝐀𝐂𝐊𝐈𝐍𝐆-𝐌𝐃

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -4,7 +4,7 @@ const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Salut je m\'appelle *HACKING-MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil ';
+    let z = 'Salut je m\'appelle *HACKING-MD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
     let d = ' developpé par *Thomas*';
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029VaYrk3lIiRozw8zeoh00';  // Remplacez cet URL par le lien que vous souhaitez envoyer
